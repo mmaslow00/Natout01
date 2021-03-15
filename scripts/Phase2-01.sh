096ec02af80014e4ffb59a530d971753b0b2840a
mmaslow00 <mmaslow00@gmail.com>
2021-03-14 19:57:12 -0700

    Add tests for file upload

--- scripts/Phase2-01.sh
+++ scripts/Phase2-01.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sfdx force:source:deploy --manifest manifest\\Phase2-01.xml --checkonly --testlevel RunSpecifiedTests --runtests NatoutTripItineraryControllerTest,NatoutTripBudgetControllerTest,NatoutEmailHandlerTest,NatoutTripPostTripReportTest --json --loglevel fatal
+sfdx force:source:deploy --manifest manifest\\Phase2-01.xml --checkonly --testlevel RunSpecifiedTests --runtests ContentManagerServiceTest,FileUploadServiceTest,NatoutTripTriggerHandlerTest,NatoutTripItineraryControllerTest,NatoutTripBudgetControllerTest,NatoutEmailHandlerTest,NatoutTripPostTripReportTest --json --loglevel fatal
