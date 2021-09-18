95bd84325c96a669ebe2bbc593b106024741963d
mmaslow00 <mmaslow00@gmail.com>
2021-09-17 20:32:09 -0700

    Sept Fix RC 1

--- force-app/main/default/lwc/natoutTripComments/natoutTripComments.js
+++ force-app/main/default/lwc/natoutTripComments/natoutTripComments.js
@@ -12,13 +12,20 @@ export default class NatoutTripComments extends LightningElement {
 
     constructor() {
         super();
-        this.checkboxOptions = [
+        this.volunteerOptions = [
             {label: 'Trip Leader and any collaborators they appoint', value: 'Leaders'},
             {label: 'Subcommittee officers and any collaborators they appoint', value: 'Officers'},
-            {label: 'National Outings staff', value: 'Staff'},
+        ];
+        this.staffOptions = [
+            {label: 'Program Director', value: 'Director'},
+            {label: 'Marketing Manager', value: 'Marketing'},
+            {label: 'Land Agency Associate', value: 'Land'},
+            {label: 'Finance Associate', value: 'Finance'},
+            {label: 'Program Safety Team', value: 'Safety'},
             {label: 'Brochure Editor', value: 'Brochure Editor'}
         ];
-        this.checkboxValues = [];
+        this.staffValues = [];
+        this.volunteerValues = [];
     }
   
     @wire(getCommentsList, {tripId: '$recordId'})
@@ -36,6 +43,9 @@ export default class NatoutTripComments extends LightningElement {
                         let name = '';
                         if(recipient.Contact__r) {
                             name = recipient.Contact__r.Name;
+                        }
+                        else if(recipient.Staff_Position__c != null) {
+                            name = recipient.Staff_Position__c;
                         }
                         else {
                             if(recipient.Email__c.indexOf('brochure') >= 0) {
@@ -61,16 +71,21 @@ export default class NatoutTripComments extends LightningElement {
             this.collabList = undefined;
         }
     }
-    handleChange(e) {
+    handleCommentChange(e) {
         this.tripComment = e.target.value;
     }
-    handleCheckboxChange(e) {
-        this.checkboxValues = e.detail.value;
+    handleVolunteerChange(e) {
+        this.volunteerValues = e.detail.value;
+    }
+    handleStaffChange(e) {
+        this.staffValues = e.detail.value;
     }
     get isModalOpen() {
         return this.modalOpen;
     }
     closeModal() {
+        this.staffValues = [];
+        this.volunteerValues = [];
         this.modalOpen = false;
     }
     openModal() {
@@ -87,15 +102,20 @@ export default class NatoutTripComments extends LightningElement {
                 fields: {
                     National_Outings_Trip__c: this.recordId,
                     Comments__c: this.tripComment,
-                    Notify_Approvers__c: (this.checkboxValues.indexOf('Officers') >= 0),
-                    Notify_Creators__c: (this.checkboxValues.indexOf('Leaders') >= 0),
-                    Notify_Staff__c: (this.checkboxValues.indexOf('Staff') >= 0),
-                    Notify_Brochure_Editor__c: (this.checkboxValues.indexOf('Brochure Editor') >= 0)
+                    Notify_Approvers__c: (this.volunteerValues.indexOf('Officers') >= 0),
+                    Notify_Creators__c: (this.volunteerValues.indexOf('Leaders') >= 0),
+                    Notify_Staff__c: (this.staffValues.indexOf('Director') >= 0),
+                    Notify_Brochure_Editor__c: (this.staffValues.indexOf('Brochure Editor') >= 0),
+                    Notify_Finance__c: (this.staffValues.indexOf('Finance') >= 0),
+                    Notify_Land_Agency__c: (this.staffValues.indexOf('Land') >= 0),
+                    Notify_Marketing__c: (this.staffValues.indexOf('Marketing') >= 0),
+                    Notify_Safety__c: (this.staffValues.indexOf('Safety') >= 0)
                 }
-            }).then(result => {
+            }).then(() => {
                 this.showSnackbar('success', 'Comment Saved');
                 this.modalOpen = false;
-                this.checkboxValues = [];
+                this.staffValues = [];
+                this.volunteerValues = [];
                 return refreshApex(this.wiredComments);
             })
             .catch(error => {
